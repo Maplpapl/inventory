7feb2c4338f97231d8850c07a254f40589029b49
Andre Saiko <s.andre1989@googlemail.com>
2012-10-10 20:01:51 +0200

    edit settings.py and urls.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 #        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 #        'NAME': 'inventar',                      # Or path to database file if using sqlite3.
 #        'USER': 'postgres',                      # Not used with sqlite3.
-#        'PASSWORD': 'rubysoho1989',              # Not used with sqlite3.
+#        'PASSWORD': '12345',              # Not used with sqlite3.
 #        'HOST': 'localhost',                     # Set to empty string for localhost. Not used with sqlite3.
 #        'PORT': '5432',                          # Set to empty string for default. Not used with sqlite3.
 #    }
--- inventory/urls.py
+++ inventory/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^inventory/', include('inventory.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(admin.site.urls)),
 )
